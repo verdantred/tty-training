9eee901c3a62df348fb9f471416f26f4dd563da3
Ville Niittunen <ww.niittunen@hotmail.com>
2016-12-03 14:37:20 +0200

    Add more fields to new tweet inserts.

--- tweetcatcher/streams/filters/index.js
+++ tweetcatcher/streams/filters/index.js
@@ -3,10 +3,11 @@ var request = require('request');
 var re = /youtube.com\/watch?./i;
 
 var streamFilter = function(tweet) {
-	console.log(chalk.green(tweet.user.screen_name, ' : ' , tweet.text));
+	console.log(chalk.yellow(tweet.user.screen_name, ' : ' , tweet.text));
 	matches = tweet.entities.urls[0].display_url.match(re);
-	if(matches){
-		var data = {message: tweet.text, url: matches[0]};
+	if(matches && tweet.retweet_count && tweet.retweet_count > 1){
+		console.log(chalk.green(tweet.text, ' : ' , matches[0], ' : ', tweet.retweet_count + 1));
+		var data = {message: tweet.text, url: matches[0], tweet_count: tweet.retweet_count + 1};
 		
 		request.post({url: '172.17.0.1:80/interface/',
 							body: data, 
