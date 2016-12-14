1523c26c8d3c814bd532646bb3660a25a8a6de00
Ville Niittunen <ww.niittunen@hotmail.com>
2016-12-14 02:28:28 +0200

    Tweak dotenv usage

--- tweetcatcher/server.js
+++ tweetcatcher/server.js
@@ -9,10 +9,10 @@ var streamParameters = {
 };
 
 var client = new Twitter({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret
+  consumer_key: consumer_key,
+  consumer_secret: consumer_secret,
+  access_token_key: access_token_key,
+  access_token_secret: access_token_secret
 });
 
 console.log("Hey, I got here!");
